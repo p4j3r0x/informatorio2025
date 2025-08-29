bd28ee1ad0acae63cb5a8db0a40385a87b5533d6
yo <paginas_01@hotmail.com>
2025-08-29 04:07:30 -0300

    anashe

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
+// desaparece después de 3 segundos
+setTimeout(() => {
+  const bienvenida = document.getElementById("bienvenida");
+  bienvenida.style.transition = "opacity 1s ease";
+  bienvenida.style.opacity = 0;
+
+  // para ocultar el elemento en html
+  setTimeout(() => {
+    bienvenida.style.display = "none";
+  }, 1000); // 1s de transición
+}, 3000); //3seg de duracion
+
+// Navbar cambia al hacer scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navegacion');
+  if (window.scrollY > 10) {
+    navbar.classList.add('activar-nav');
+  } else {
+    navbar.classList.remove('activar-nav');
+  }
 });
