cffa9110e89a67ed6a3626640185f5446bc41382
yo <paginas_01@hotmail.com>
2025-08-30 06:18:11 -0300

    asd

---
+++ nuevoapp.js
@@ -0,0 +1,22 @@
+// Desaparece la pantalla de bienvenida después de 3 segundos
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        const bienvenida = document.getElementById("bienvenida");
+        bienvenida.style.transition = "opacity 1s ease";
+        bienvenida.style.opacity = 0;
+
+        setTimeout(() => {
+            bienvenida.style.display = "none";
+        }, 1000);
+    }, 3000);
+});
+
+// Navbar aparece al hacer scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navegacion');
+    if (window.scrollY > 10) {
+        navbar.classList.add('activar-nav');
+    } else {
+        navbar.classList.remove('activar-nav');
+    }
+});
